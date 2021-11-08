ecc8041490bb56d9f702db6385124e4fc598bd82
kimsichan <ksc970122@naver.com>
2021-11-08 17:18:30 +0900

    Update ClassScoreEx1.java

--- si/src/D9/ClassScoreEx1.java
+++ si/src/D9/ClassScoreEx1.java
@@ -1,17 +1,31 @@
 package D9;
 
+import java.util.Scanner;
+
 public class ClassScoreEx1 {
 
 	public static void main(String[] args) {
 	/* 국어, 영어, 수학 성적을 관리하기 위한 클래스를 만들고,
 	 * 학생 5명의 성적을 관리하기 위한 배열을 만드세요.*/
-		Score score[]=new Score[5];
+		//5명의 학생 정보를 저장할 수 있는 배열을 생성
+		Score score[]=new Score[3];
+		Scanner scan = new Scanner(System.in);
 		for(int i = 0 ; i < score.length; i++) {
-			score[i] = new Score();
+			//new를 통해 학생 정보를 생성하고, 생성자로 초기화
+			System.out.print("이름 : ");
+			String name = scan.next();
+			System.out.print("국어 : ");
+			int kor = scan.nextInt();
+			System.out.print("영어 : ");
+			int eng = scan.nextInt();
+			System.out.print("수학 : ");
+			int math = scan.nextInt();
+			score[i] = new Score(kor,eng,math,name);
 		}
 		for(Score tmp : score) {
 			tmp.prinInfo();
 		}
+		scan.close();
 	}
 
 }
